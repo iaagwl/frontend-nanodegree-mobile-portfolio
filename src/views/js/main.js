e506b22a5652fdb747fb12673ece8675058c14c9
Isac <iaagwl@gmail.com>
2017-05-03 23:34:00 +0200

    fix updatePositions function

--- src/views/js/main.js
+++ src/views/js/main.js
@@ -501,10 +501,16 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
-  for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+  // Calculate phases before updating the styles
+  var phases = [];
+  var top = document.body.scrollTop / 1250;
+  for (var i = 0; i < 5; i++) {
+    phases.push(100 * Math.sin(top + i));
+  }
+
+  // Update styles
+  for (i = 0; i < items.length; i++) {
+    items[i].style.left = (items[i].basicLeft + phases[i % 5]) + 'px';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
@@ -517,22 +523,33 @@ function updatePositions() {
   }
 }
 
+var items = [];
+
 // runs updatePositions on scroll
 window.addEventListener('scroll', updatePositions);
 
 // Generates the sliding pizzas when the page loads.
 document.addEventListener('DOMContentLoaded', function() {
-  var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
-    var elem = document.createElement('img');
+  var cols = 8;
+  var maxPizzaRows = Math.ceil(window.innerHeight / s);
+  var elem;
+  for (var i = 0; i < maxPizzaRows * cols; i++) {
+    elem = document.createElement('img');
     elem.className = 'mover';
-    elem.src = "images/pizza.png";
+    elem.src = "images/pizza-half.png";
     elem.style.height = "100px";
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.querySelector("#movingPizzas1").appendChild(elem);
+
+	items.push(elem);
   }
+
+  var movingPizzaParent = document.querySelector("#movingPizzas1");
+  for (i = 0; i < items.length; i++) {
+    movingPizzaParent.appendChild(items[i]);
+  }
+
   updatePositions();
 });
